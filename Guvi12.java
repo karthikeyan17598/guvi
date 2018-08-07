ce9680b72ad79dca203d40827955ba7e46ee3227
karthikeyan17598 <40016110+karthikeyan17598@users.noreply.github.com>
2018-08-07 14:51:17 +0530

    Create Guvi12.java

---
+++ Guvi12.java
@@ -0,0 +1,15 @@
+package guviproblem;
+
+import java.util.Arrays;
+
+public class Guvi12 {
+
+	public static void main(String[] args) {
+		int arr[]= {1,2,3,4,5,6};
+		Arrays.sort(arr);
+		for(int i=0;i<arr.length;i++) {
+		}
+		System.out.println(arr[arr.length-2]);
+	}
+
+}
