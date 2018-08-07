c7d3be537c4307ad12fdaee217d0a8327b3cbbed
karthikeyan17598 <40016110+karthikeyan17598@users.noreply.github.com>
2018-08-07 15:08:00 +0530

    Update Guvi15.java

--- Guvi15.java
+++ Guvi15.java
@@ -5,7 +5,7 @@ public class Guvi15 {
 	public static void main(String[] args) {
 		int sum=0;
 		int arr[]= {1,2,3,4,5};
-		for(int i=0;i<arr.length;i++) {
+		for(int i=1;i<arr.length;i++) {
 			 sum = sum+arr[i];
 		}
 		System.out.println(sum);
