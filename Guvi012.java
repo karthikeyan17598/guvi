2fc0225572bf180a748e6f66dd1c149b895ce25c
karthikeyan17598 <40016110+karthikeyan17598@users.noreply.github.com>
2019-05-31 11:39:31 +0530

    Update and rename Guvi12.java to Guvi012.java

---
+++ Guvi012.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Guvi012 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a,b,c=0;
+		Scanner sc=new Scanner (System.in);
+		System.out.print("Enter the number");
+		int n=sc.nextInt();
+		int temp=n;
+		while(n>0) {
+			a=n%10;
+			c=(c*10)+a;
+			n=n/10;
+		}
+		if(temp==c) {
+			System.out.println("palindrome");
+		}
+		else
+		{
+		System.out.println("not palindrome");
+		}
+
+	}
+
+}
